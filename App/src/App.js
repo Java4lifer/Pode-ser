96013c88fbad9b12d65170eaf6dd859a5e4d8a94
Java4lifer <104568501+Java4lifer@users.noreply.github.com>
2023-09-26 18:21:09 +0000

    random

--- App/src/App.js
+++ App/src/App.js
@@ -3,12 +3,14 @@ import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import style from "./style.js"
 import Cirno from "./components/Cirno.js"
 import First from "./components/component1.js"
+import Racism from './components/Racism.js';
 //import First, {func1, func2}
 
 export default function App() {
   return (
     <View style={styles.app}>
       <Text>Be racist.</Text>
+      <Racism racistLv={21} min={0} max={60}/>
       <Cirno/>
       <First style={styles.container}/>
     </View>
